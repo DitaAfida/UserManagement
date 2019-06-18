8e122fa111a113ce5db17a7b4fc67b82e021e0fb
Dita Afida <ditaafida@gmail.com>
2019-06-18 16:25:58 +0700

    Context

--- DataAccess/Context/MyContext.cs
+++ DataAccess/Context/MyContext.cs
@@ -11,19 +11,19 @@ namespace DataAccess.Context
     public class MyContext : DbContext
     {
         public MyContext() : base ("MyContext") { }
-        public DbSet<Aplication> Aplication { get; set; }
-        public DbSet<Department> Department { get; set; }
-        public DbSet<District> District { get; set; }
-        public DbSet<Employee> Employee { get; set; }
-        public DbSet<Position> Position { get; set; }
-        public DbSet<Province> Province { get; set; }
-        public DbSet<Regency> Regency { get; set; }
-        public DbSet<Religion> Religion { get; set; }
-        public DbSet<Role> Role { get; set; }
-        public DbSet<User> User { get; set; }
-        public DbSet<UserApplication> UserApplication { get; set; }
-        public DbSet<UserRole> UserRole { get; set; }
-        public DbSet<Village> Village { get; set; }
+        public DbSet<Aplication> Aplications { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<District> Districts { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Province> Provinces { get; set; }
+        public DbSet<Regency> Regencys { get; set; }
+        public DbSet<Religion> Religions { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserApplication> UserApplications { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<Village> Villages { get; set; }
 
     }
 }
